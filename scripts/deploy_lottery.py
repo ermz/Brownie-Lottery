13d37074f18a9f576dbb2eea6de0469b6d411864
ermz <eramirez4369@gmail.com>
2022-05-23 00:36:53 -0400

    Finished enter_lotttery, working on end_lottery on helpful_scripts.py

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -2,7 +2,7 @@ from scripts.helpful_scripts import get_account, get_contract
 from brownie import Lottery, config, network
 
 def deploy_lottery():
-    account = get_account(id="local_deployment_account")
+    account = get_account()
     lottery = Lottery.deploy(
         get_contract("eth_usd_price_feed").address,
         get_contract("vrf_coordinator").address,
@@ -16,12 +16,36 @@ def deploy_lottery():
     return lottery
 
 def start_lottery():
-    account = get_account(id="local_deployment_account")
+    account = get_account()
     lottery = Lottery[-1]
     starting_tx = lottery.startLottery({"from": account})
     starting_tx.wait(1)
     print("Lottery started!!")
 
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    starting_tx = lottery.startLottery({"from": account})
+    starting_tx.wait(1)
+    print("Lottery has started")
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery!")
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    # end_tx = lottery.endLottery({"from": account})
+    # end_tx.wait(1)
+    # print("You have ended the lottery")
+    # Need to provide Link Token in order to run
+
 def main():
     deploy_lottery()
     start_lottery()
+    enter_lottery()
